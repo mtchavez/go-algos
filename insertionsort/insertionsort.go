cf8332f99554139b40fe92fd24c4c1e447d4a488
Chavez <nailsbrokeandfell@gmail.com>
2014-09-14 21:45:31 -0700

    Reverse insertion sort

--- insertionsort/insertionsort.go
+++ insertionsort/insertionsort.go
@@ -11,3 +11,15 @@ func Sort(list []int) []int {
 	}
 	return list
 }
+
+func SortReverse(list []int) []int {
+	for j := 1; j < len(list); j++ {
+		key := list[j]
+		i := j - 1
+		for ; i >= 0 && list[i] < key; i-- {
+			list[i+1] = list[i]
+		}
+		list[i+1] = key
+	}
+	return list
+}
--- insertionsort/insertionsort_test.go
+++ insertionsort/insertionsort_test.go
@@ -19,7 +19,7 @@ func TestSortUnsorted(t *testing.T) {
 	expected := []int{1, 2, 4, 6, 43, 78, 99, 232}
 	sorted := Sort(list)
 	if !reflect.DeepEqual(sorted, expected) {
-		t.Errorf("Expected sorted list to not be changed, got: %x", sorted)
+		t.Errorf("Expected list to be sorted, got: %x", sorted)
 	}
 }
 
@@ -28,7 +28,7 @@ func TestSortEmpty(t *testing.T) {
 	expected := []int{}
 	sorted := Sort(list)
 	if !reflect.DeepEqual(sorted, expected) {
-		t.Errorf("Expected sorted list to not be changed, got: %x", sorted)
+		t.Errorf("Expected empty list, got: %x", sorted)
 	}
 }
 
@@ -37,6 +37,15 @@ func TestSortOneValue(t *testing.T) {
 	expected := []int{632}
 	sorted := Sort(list)
 	if !reflect.DeepEqual(sorted, expected) {
-		t.Errorf("Expected sorted list to not be changed, got: %x", sorted)
+		t.Errorf("Expected single value list, got: %x", sorted)
+	}
+}
+
+func TestSortReverse(t *testing.T) {
+	list := []int{4, 1, 78, 2, 6, 232, 99, 43}
+	expected := []int{232, 99, 78, 43, 6, 4, 2, 1}
+	sorted := SortReverse(list)
+	if !reflect.DeepEqual(sorted, expected) {
+		t.Errorf("Expected list to be sorted in reverse, got: %x", sorted)
 	}
 }
