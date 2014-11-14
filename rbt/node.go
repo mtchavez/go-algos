9e6e17d01c1d44f4822f349a7e2f89b0883e40e3
Chavez <matthew@el-chavez.me>
2014-11-13 21:24:21 -0800

    RBT node replace function

--- rbt/node.go
+++ rbt/node.go
@@ -34,3 +34,16 @@ func (n *node) setChildren(x, y *node) {
 	n.setLeft(x)
 	n.setRight(y)
 }
+
+func (n *node) replace(y *node) {
+	if y.parent == nil {
+		if y != nil {
+			y.parent = nil
+		}
+	} else if n.parent.left == n {
+		n.parent.setLeft(y)
+	} else {
+		n.parent.setRight(y)
+	}
+	n.parent = nil
+}
